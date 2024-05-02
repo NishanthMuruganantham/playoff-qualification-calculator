fe721985c7f7c5c3e20b68f0acd5ef06b1b3f7a0
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-05-02 11:58:10 +0530

    initial_sidebar_state has been expanded

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from src.views.custom_schedule.custom_schedule import simulate_the_qualification
 from src.views.homepage import homepage
 from src.views.tournament import simulate_for_ipl
 
-st.set_page_config(page_title="Playoff Qualification Scenario Generator", page_icon=":soccer:",layout="wide")
+st.set_page_config(
+    initial_sidebar_state="expanded",
+    layout="wide",
+    page_title="Playoff Qualification Scenario Generator",
+    page_icon=":soccer:",
+)
 
 
 page_names_to_funcs = {
     "Home": homepage,
     "Indian Premier League (IPL)": simulate_for_ipl,
-    "custom": simulate_the_qualification_for_custom_schedule
+    "Custom Schedule": simulate_the_qualification_for_custom_schedule
 }
 
 demo_name = st.sidebar.selectbox("Pages", page_names_to_funcs.keys())
