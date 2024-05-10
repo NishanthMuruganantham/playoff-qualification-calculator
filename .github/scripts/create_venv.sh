215df9b30dcf126c5d341ee650203ac73ffe5163
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-05-10 17:34:35 +0530

    pip has been corrected

--- .github/scripts/create_venv.sh
+++ .github/scripts/create_venv.sh
@@ -5,4 +5,4 @@ VENV=${1:-venv}
 python3.9 -m venv $VENV && \
   source $VENV/bin/activate && \
   python3.9 -m pip install pip==23.3.1 && \
-  pip install pip install requirements.txt
+  pip install pip install -r requirements.txt
