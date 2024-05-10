d7836799c99474dc5cc16b85b3e6ab8fd84acd1c
NishanthMuruganantham <nishanthmurugananth10@gmail.com>
2024-05-10 17:32:46 +0530

    packages have been included

--- .github/scripts/create_venv.sh
+++ .github/scripts/create_venv.sh
@@ -4,4 +4,5 @@ VENV=${1:-venv}
 
 python3.9 -m venv $VENV && \
   source $VENV/bin/activate && \
-  python3.9 -m pip install pip==23.3.1
+  python3.9 -m pip install pip==23.3.1 && \
+  pip install pip install requirements.txt
