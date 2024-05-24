3753a52a3924af6b9769949b33062799be66780d
Nishanth Muruganantham <82966884+NishanthMuruganantham@users.noreply.github.com>
2024-05-24 12:14:11 +0530

    Displaying error message when the tournament's playoff or tournament is over

--- src/views/tournament.py
+++ src/views/tournament.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from points_table_simulator import PointsTableSimulator
+from points_table_simulator.exceptions import AllMatchesCompletedError
 from src.functions.fixture_collector import (
     fetch_points_table_for_given_tournament,
     get_fixture_for_given_tournament
@@ -19,18 +20,22 @@ def simulate_for_ipl():
     _apply_banner_styles()
     _create_banner(subtitle="For Indian Premier League")
     st.write("")
-    with st.balloons():
-        tournament_df = get_fixture_for_given_tournament(1410320)
-        points_table_simulator = PointsTableSimulator(
-            tournament_schedule=tournament_df,
-            points_for_a_win=2,
-            tournament_schedule_away_team_column_name="team_2",
-            tournament_schedule_home_team_column_name="team_1",
-        )
-        _display_given_fixture_and_current_points_table(
-            current_points_table=fetch_points_table_for_given_tournament(1410320),
-            expanded=True,
-            remaining_fixture=tournament_df,
-        )
+    try:
+        with st.balloons():
+            tournament_df = get_fixture_for_given_tournament(1410320)
+            points_table_simulator = PointsTableSimulator(
+                tournament_schedule=tournament_df,
+                points_for_a_win=2,
+                tournament_schedule_away_team_column_name="team_2",
+                tournament_schedule_home_team_column_name="team_1",
+            )
+            _display_given_fixture_and_current_points_table(
+                current_points_table=fetch_points_table_for_given_tournament(1410320),
+                expanded=True,
+                remaining_fixture=tournament_df,
+            )
 
-    _generate_qualification_scenarios(points_table_simulator)
+        _generate_qualification_scenarios(points_table_simulator)
+
+    except AllMatchesCompletedError:
+        st.error("All league matches in the tournament are completed.")
